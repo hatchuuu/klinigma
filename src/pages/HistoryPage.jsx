5de693787a8047ffebcf5dede10cfb18b3a7417d
Ryan Kusuma <ryankusuma147@gmail.com>
2024-12-23 07:34:00 +0700

    [PROD] Error Handling

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
